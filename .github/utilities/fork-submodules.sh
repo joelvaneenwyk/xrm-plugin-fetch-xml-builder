732445092f4f264c5d3d8e94d599211d18a5eaaa
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-12-22 10:09:41 -0800

    Replace submodules with personal forks of the repositories (#1)
    * Replace submodules with unique forks of the repositories
    * Add helper script '.github/utilities/fork-submodules.sh' for forking submodules on GitHub

---
+++ .github/utilities/fork-submodules.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function fork_submodules() {
+    submodules="$(git config --file .gitmodules --get-regexp path | awk '{ print $2 }')"
+    for submodule in $submodules; do
+        (
+            cd "$submodule" || true
+            owner=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///' | sed 's/\.git$//' | cut -d/ -f1)
+            repo=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///' | sed 's/\.git$//' | cut -d/ -f2)
+            gh repo fork "$owner/$repo" --clone=false
+        )
+    done
+}
+
+function main() {
+    fork_submodules
+    git submodule update --init --recursive
+}
+
+main
